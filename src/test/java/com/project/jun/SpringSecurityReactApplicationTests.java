231aae57e6d81f3790a0d168934ecbdb16e5089a
KangJunWoo <jwkang5665@gmail.com>
2021-08-05 15:38:58 +0900

    ..

--- src/test/java/com/project/jun/SpringSecurityReactApplicationTests.java
+++ src/test/java/com/project/jun/SpringSecurityReactApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecondProjectApplicationTests {
+class SpringSecurityReactApplicationTests {
 
 	@Test
 	void contextLoads() {
